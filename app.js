bd5fc4fb9535df5cd91caa7176fc310173264f6a
Alec Davidson <davidson9076@gmail.com>
2018-03-19 17:20:51 -0500

    adding port

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const app = express()
 
 app.use(express.static('public'))
 app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(3000, () => console.log('app listening on port 3000!'))
+var PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log('app listening on port 3000!'))
